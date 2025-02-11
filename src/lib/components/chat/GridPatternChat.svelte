a83159bc9733e615a3a4116dbacb082bc3b6ac93
Ivan <ivangyr321@gmail.com>
2025-02-11 00:33:34 -0500

    agregar componente GridPatternChat y reorganizar la estructura del layout con encabezado y pie de página

---
+++ src/lib/components/chat/GridPatternChat.svelte
@@ -0,0 +1,31 @@
+<script>
+	let opacity = '0.1';
+	let color = 'rgb(30, 58, 138)';
+	let size = '20px';
+	let thickness = '1px';
+</script>
+
+<div
+	class="pointer-events-none absolute inset-0"
+	style:opacity
+	style:--pattern-color={color}
+	style:--pattern-size={size}
+	style:--line-thickness={thickness}
+></div>
+
+<style>
+	div {
+		background-size: var(--pattern-size) var(--pattern-size);
+		background-image:
+			linear-gradient(
+				to right,
+				var(--pattern-color) var(--line-thickness),
+				transparent var(--line-thickness)
+			),
+			linear-gradient(
+				to bottom,
+				var(--pattern-color) var(--line-thickness),
+				transparent var(--line-thickness)
+			);
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
+	import FooterChat from '$lib/components/chat/FooterChat.svelte';
+	import GridPatternChat from '$lib/components/chat/GridPatternChat.svelte';
+	import HeaderChat from '$lib/components/chat/HeaderChat.svelte';
+
 	import '../app.css';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex min-h-screen flex-col">
+	<HeaderChat />
+
+	<main class="relative flex-1 flex-col py-4">
+		<GridPatternChat />
+
+		{@render children()}
+	</main>
+
+	<FooterChat />
+</div>
