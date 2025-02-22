9b8778ee903107aef92d136f20320911ae3652c3
Ivan <ivangyr321@gmail.com>
2025-02-21 21:45:10 -0500

    mejorar: agregar un diálogo de información en el encabezado del chat y un nuevo ícono de información

--- src/lib/components/chat/HeaderChat.svelte
+++ src/lib/components/chat/HeaderChat.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
-	import { NewConversationIcon } from '$lib/icons';
+	import { InfoIcon, NewConversationIcon } from '$lib/icons';
+	import { fade } from 'svelte/transition';
+
+	let infoDialog: HTMLDialogElement = $state() as HTMLDialogElement;
+
+	let showModal = $state(false);
+
+	const openDialog = () => {
+		showModal = true;
+	};
+
+	const closeDialog = () => {
+		showModal = false;
+	};
 </script>
 
 <header class="bg-white shadow-lg">
@@ -23,17 +36,66 @@
 				</figcaption>
 			</figure>
 
-			<form action="?/reset" method="POST" use:enhance>
+			<section class="flex gap-4">
+				<form action="?/reset" method="POST" use:enhance>
+					<button
+						type="submit"
+						class="text-primary border-primary flex cursor-pointer items-center gap-2 rounded-lg border p-2 transition-all duration-200 hover:bg-gray-100 hover:shadow-md"
+						aria-label="Nueva Consulta"
+						title="Nueva Consulta"
+					>
+						<span class="hidden text-sm sm:inline">Nueva Consulta</span>
+						<NewConversationIcon />
+					</button>
+				</form>
+
 				<button
-					type="submit"
 					class="text-primary border-primary flex cursor-pointer items-center gap-2 rounded-lg border p-2 transition-all duration-200 hover:bg-gray-100 hover:shadow-md"
-					aria-label="Nueva Consulta"
-					title="Nueva Consulta"
+					onclick={openDialog}
 				>
-					<span class="hidden sm:inline">Nueva Consulta</span>
-					<NewConversationIcon />
+					<span class="hidden text-sm sm:inline">Información</span>
+					<InfoIcon />
 				</button>
-			</form>
+			</section>
 		</div>
 	</div>
 </header>
+
+{#if showModal}
+	<dialog
+		bind:this={infoDialog}
+		class="bg-primary/10 z-40 flex h-full w-full items-center justify-center backdrop-blur-lg"
+		transition:fade={{ duration: 200 }}
+	>
+		<article class="mx-2 max-w-sm rounded-lg bg-white p-6 shadow-lg">
+			<header>
+				<h2 class="text-primary text-xl font-bold">Información Importante</h2>
+			</header>
+
+			<main class="my-4 space-y-4">
+				<p class="text-sm text-gray-700">
+					Este asistente virtual utiliza tecnología de inteligencia artificial y aunque se esfuerza
+					por proporcionar información precisa, puede cometer errores. La información proporcionada
+					es solo para orientación general.
+				</p>
+
+				<a
+					class="block text-sm"
+					href="https://vanchi.pro"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Desarrollado por <span class="text-secondary font-semibold">Ivan</span>
+				</a>
+			</main>
+
+			<button
+				type="button"
+				onclick={closeDialog}
+				class="bg-secondary hover:bg-secondary/90 w-full cursor-pointer rounded py-2 text-sm text-white"
+			>
+				Entendido
+			</button>
+		</article>
+	</dialog>
+{/if}
--- src/lib/icons/index.ts
+++ src/lib/icons/index.ts
@@ -1,2 +1,3 @@
 export { default as NewConversationIcon } from './NewConversation.svelte';
 export { default as SendMessageIcon } from './SendMessage.svelte';
+export { default as InfoIcon } from './Info.svelte';
